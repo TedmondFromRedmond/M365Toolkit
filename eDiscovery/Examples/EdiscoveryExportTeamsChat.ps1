f3bf73a0dff1623031a6eac24ae17e33775a4065
Tedmond From Redmond <149241560+TedmondFromRedmond@users.noreply.github.com>
2023-11-30 01:17:44 -0800

    Add files via upload

---
+++ eDiscovery/Examples/EdiscoveryExportTeamsChat.ps1
@@ -0,0 +1,70 @@
+<#
+To export Teams chat using eDiscovery with PowerShell, you need to use the Security & Compliance Center PowerShell module. Before running the script, ensure you have the necessary permissions in the Security & Compliance Center to perform eDiscovery tasks.
+
+Here's an outline of the steps you'll follow in PowerShell:
+
+Connect to Security & Compliance Center PowerShell.
+Create a new eDiscovery case.
+Add a hold to the case if necessary.
+Run a search on the case.
+Export the search results.
+Below is a simplified example of how the PowerShell script might look:
+#>
+# Import the Exchange Online Management module or install if not present
+if (!(Get-Module -ListAvailable -Name ExchangeOnlineManagement)) {
+    Install-Module -Name ExchangeOnlineManagement
+}
+
+# Connect to the Security & Compliance Center
+Connect-IPPSSession -UserPrincipalName "your_admin_account@yourdomain.com"
+
+# Create a new eDiscovery case
+$caseName = "TeamsChatExportCase"
+New-ComplianceCase -Name $caseName
+
+# Optionally, place content locations on hold
+$holdName = "TeamsChatHold"
+$teamChatContentLocation = "TeamsChatLocationHere"
+New-CaseHoldPolicy -Name $holdName -Case $caseName -ContentLocation $teamChatContentLocation
+New-CaseHoldRule -Policy $holdName -ContentMatchQuery "c:c"
+
+# Run a search on the case
+$searchName = "TeamsChatSearch"
+New-ComplianceSearch -Name $searchName -Case $caseName -TeamsChatLocations $teamChatContentLocation
+Start-ComplianceSearch -Identity $searchName
+
+# Wait for the search to complete - this is a simplified wait loop
+do {
+    Start-Sleep -Seconds 30
+    $searchStatus = Get-ComplianceSearch -Identity $searchName
+} while ($searchStatus.Status -ne "Completed")
+
+# Export the search results
+$exportName = "TeamsChatExport"
+New-ComplianceSearchAction -SearchName $searchName -Export -ExportName $exportName
+
+# Check the status of the export
+$exportStatus = Get-ComplianceSearchAction -Identity "$searchName\_Export"
+while ($exportStatus.Status -ne "Completed") {
+    Start-Sleep -Seconds 30
+    $exportStatus = Get-ComplianceSearchAction -Identity "$searchName\_Export"
+}
+
+# Display the export results
+$exportStatus.ResultsLocation
+
+<#
+Note:
+Note:
+
+Replace "your_admin_account@yourdomain.com" with your actual admin account.
+Replace "TeamsChatLocationHere" with the actual content location identifier of the Teams chat.
+This script assumes you're already set up with the necessary permissions and modules.
+The content match query ("c:c") in the New-CaseHoldRule command is a placeholder. You would replace this with your actual query parameters to specify the chat messages you want to export.
+This script is a high-level example and does not handle every possible situation you may encounter. Please tailor the script to fit your organization's specific requirements, and ensure you understand and comply with all relevant laws and policies related to eDiscovery.
+
+Remember to run these types of scripts in a test environment first and validate that they work as expected before running them in a production environment. Always handle the exported data securely and in compliance with your organization's data governance policies.
+
+#>
+
+
